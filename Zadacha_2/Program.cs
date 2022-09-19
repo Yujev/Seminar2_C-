1eb8fa723d42aa807db691cdeb5606153e464f6c
Yujev <ustatar@ya.ru>
2022-09-19 20:50:40 +0300

    add Zadacha_2

---
+++ Zadacha_2/Program.cs
@@ -0,0 +1,6 @@
+﻿// Необходимо сгенерировать случайное трехзначное число и удалить вторую цифру этого числа
+int number = new Random().Next(100, 1000);
+int number1 = number / 100;
+int number3 = number % 10;
+Console.Write($"Первое число {number}, Сгенерировали число {number1}{number3}");
+
